ee35459ee8eab7c810b6096a92a03b9185525aa9
Boaz Turner <boazturner@Boazs-MacBook-Air.local>
2024-03-24 11:23:27 +0100

    fix phone number links in locations

--- frontend/src/util/whatsapp.ts
+++ frontend/src/util/whatsapp.ts
@@ -5,3 +5,21 @@ export const generateWALink = (message: string, number?: string) => {
   const phoneNumber = number || dfaultPhoneNumber
   return `${baseLink}/${phoneNumber}/?text=${encodeURIComponent(message)}`
 }
+
+type PhoneType = 'landline' | 'mobile' | null
+export const checkPhoneNumberType = (phoneNumber: string): PhoneType => {
+  // Check if it's a mobile phone number pattern
+  const isMobilePattern = /^(05[0-8])\d{7}$/.test(phoneNumber)
+  if (isMobilePattern) return 'mobile'
+
+  // Check if it's a landline phone number pattern
+  const isLandlinePattern = /^(02|03|04|08|09)\d{7}$/.test(phoneNumber)
+  if (isLandlinePattern) return 'landline'
+
+  //If not match pattern match based on length
+  if (phoneNumber.length === 9 && phoneNumber[0] === '0') return 'mobile'
+  if (phoneNumber.length === 10 && phoneNumber[0] === '0') return 'landline'
+
+  // If not a phone number
+  return null
+}
